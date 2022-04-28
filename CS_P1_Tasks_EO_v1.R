19ba34d91e6e9d354d9f95dd5e301d4ed2e68db6
Ezgi Osmanoglu <osmanoglluezgi@gmail.com>
2022-04-28 10:21:11 +0200

    All tasks, graphs will be fixed

--- CS_P1_Tasks_EO_v1.R
+++ CS_P1_Tasks_EO_v1.R
@@ -24,7 +24,7 @@ project1data <- cbind(X2022_02,project1data_diff)
 project1data_final <- project1data[,c(1,3,7:12)]
 head(project1data_final,10)
 
-#Trends
+#Task 1
 plot_file<-ts(project1data_final,start=1959, frequency = 4)
 
 autoplot(plot_file)+
@@ -66,52 +66,94 @@ autoplot(plot_file[,c(9,10,12)])+
 
 #ACF (Check the paper that database coming from and others)
 
-acf(GDP[-1], main = "ACF of the GDP growth")
+acf(na.omit(project1data_final$GDPC1), main = "ACF of the GDP growth")
 
 
 #Task 2
 library(xts)
-project1data_gdp <- project1data_final[3:252,5]
+project1data_gdp <- project1data_final[2:252,5]
 
-project1data_model<-  ts(project1data_gdp,start=c(1959,3), freq = 4)
+project1data_model<-  ts(project1data_gdp,start=c(1959,2), freq = 4)
 
-is.ts(project1data_model)
 print(project1data_model)              
 
 plot.ts(project1data_model, main="Growth in GDP", ylab="%GDPC1")
 
-gdpc1_ar <- arima(project1data_model , order = c(1, 0, 0))
-gdpc1_ar
-residuals <- residuals(gdpc1_ar)
-gdpc1_fitted <- project1data_model-residuals
-
-ts.plot(project1data_model)
-points(gdpc1_fitted, type = "l", col = 2, lty = 2)
-#plot(decompose(project1data_model))
 
 #AR Model
 
-GDP <- xts(project1data_final$`%GDPC1`[-1], project1data_final$sasdate[-1], order.by=as.Date(project1data_final$sasdate[-1], format = "%m/%d/%Y"))
+arfc <- c()
+for (i in 2:250) {
+armod1 <- arima(project1data_model[1:i], c(1,0,0))
+forecasts <- forecast(armod1, 1)
+arfc <- append(arfc,forecasts$mean)
+}
 
-armod1 <- arima(GDP, c(1,0,0))
-fitted(armod1)
-ts.plot(project1data_final$`%GDPC1`[-c(1:2)])
-points(fitted(armod1), type = "l", col = 2, lty = 2)
+ts.plot(project1data_model[3:251])
+points(arfc, type = "l", col = 2, lty = 2)
 
-rmse(project1data_final$`%GDPC1`[-1],fitted(armod1))
-summary(armod1)$sigma
+rmse(project1data_model[2:250],arfc)
 
+# task c
 # VAR(1) model
 
 library(vars)
-projectdata_var <- project1data_final[,c(1,3,4,6,9:12)]
 
-GDPVAR <- xts( projectdata_var[-1,-1],projectdata_var$sasdate[-1],order.by=as.Date(projectdata_var$sasdate[-1], format = "%m/%d/%Y"))
-varmod <- VAR(GDPVAR, p = 1, type = "const", season = NULL, exog = NULL) 
-summary(varmod)
-fitted(varmod)[,4]
+GDPVAR <- xts( project1data_final[-1,-1],project1data_final$sasdate[-1],order.by=as.Date(project1data_final$sasdate[-1], format = "%m/%d/%Y"))
 
-ts.plot(GDPVAR$`%GDPC1`[-1])
-points(fitted(varmod)[,4], type = "l", col = 2, lty = 2)
-rmse(project1data_final$`%GDPC1`[-c(1:2)],fitted(varmod)[,4]) #??
+varfc <- c()
+for (i in 9:250) {
+  varmod <- VAR(GDPVAR[1:i], p = 1, type = "const", season = NULL, exog = NULL) 
+  forecasts <- predict(varmod, n.ahead=1)
+  varfc <- append(varfc,forecasts$fcst$GDPC1[1])
+}
+
+ts.plot(project1data_model[9:251])
+points(varfc, type = "l", col = 2, lty = 2)
+
+rmse(project1data_model[9:250],varfc)
+
+# task d
+library(lmtest)
+varmod_gr <- VAR(GDPVAR[1:251], p = 1, type = "const", season = NULL, exog = NULL)
+grangertest(GDPVAR$GDPC1~GDPVAR$CUMFNS)
+causality(varmod_gr, cause = "CUMFNS")
+
+grangertest(GDPVAR$GDPC1~GDPVAR$UNRATESTx) #yes
+causality(varmod_gr, cause = "UNRATESTx")
+
+
+grangertest(GDPVAR$GDPC1~GDPVAR$FEDFUNDS)
+causality(varmod_gr, cause = "FEDFUNDS")
+
+
+grangertest(GDPVAR$GDPC1~GDPVAR$CPIAUCSL) #yes
+causality(varmod_gr, cause = "CPIAUCSL")
+
+
+grangertest(GDPVAR$GDPC1~GDPVAR$M1REAL) #yes
+causality(varmod_gr, cause = "M1REAL")
+
+
+grangertest(GDPVAR$GDPC1~GDPVAR$`S&P 500`) #yes
+causality(varmod_gr, cause = "S.P.500")
+
+#task e
+
+VARselect(GDPVAR, type= "const", lag.max = 10)
+varfc3 <- c()
+for (i in 25:250) {
+  varmod3 <- VAR(GDPVAR[1:i], p = 3, type = "const", season = NULL, exog = NULL) 
+  forecasts3 <- predict(varmod3, n.ahead=1)
+  varfc3 <- append(varfc3,forecasts3$fcst$GDPC1[1])
+}
+
+rmse(project1data_model[25:250],varfc3)
+
+ts.plot(project1data_model[1:251])
+points(arfc, type = "l", col = 2, lty = 2)
+points(varfc, type = "l", col = 3, lty = 2)
+points(varfc, type = "l", col = 4, lty = 2)
+
+
 
