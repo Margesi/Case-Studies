e96493b4238cc140089f407da14fba6e5a0097a3
Ezgi Osmanoglu <osmanoglluezgi@gmail.com>
2022-04-30 22:11:55 +0200

    New tasks

--- CS_P1_Tasks_EO_v1.R
+++ CS_P1_Tasks_EO_v1.R
@@ -92,7 +92,9 @@ arfc <- append(arfc,forecasts$mean)
 ts.plot(project1data_model[3:251])
 points(arfc, type = "l", col = 2, lty = 2)
 
-rmse(project1data_model[2:250],arfc)
+data_forecast_eo <- data.frame(project1data_final$sasdate[3:251],project1data_final$GDPC1[3:251], arfc)
+
+rmse(project1data_model[3:251],arfc)
 
 # task c
 # VAR(1) model
@@ -111,32 +113,47 @@ for (i in 9:250) {
 ts.plot(project1data_model[9:251])
 points(varfc, type = "l", col = 2, lty = 2)
 
-rmse(project1data_model[9:250],varfc)
+data_var_forecast_eo <- data.frame(project1data_final$sasdate[10:251],project1data_final$GDPC1[10:251],varfc)
 
+rmse(project1data_model[10:251],varfc)
+sqrt(mean((project1data_model[10:251] - varfc)^2))
 # task d
 library(lmtest)
 varmod_gr <- VAR(GDPVAR[1:251], p = 1, type = "const", season = NULL, exog = NULL)
+
+coefs <- rbind(varmod_gr$varresult$UNRATESTx$coefficients,
+varmod_gr$varresult$CUMFNS$coefficients,
+varmod_gr$varresult$FEDFUNDS$coefficients,
+varmod_gr$varresult$GDPC1$coefficients,
+varmod_gr$varresult$CPIAUCSL$coefficients,
+varmod_gr$varresult$M1REAL$coefficients,
+varmod_gr$varresult$S.P.500$coefficients)
+
 grangertest(GDPVAR$GDPC1~GDPVAR$CUMFNS)
 causality(varmod_gr, cause = "CUMFNS")
+coefs[c(4,2),c(4,2)]
 
 grangertest(GDPVAR$GDPC1~GDPVAR$UNRATESTx) #yes
 causality(varmod_gr, cause = "UNRATESTx")
+coefs[c(4,1),c(4,1)]
 
+x <- summary(varmod)
 
 grangertest(GDPVAR$GDPC1~GDPVAR$FEDFUNDS)
 causality(varmod_gr, cause = "FEDFUNDS")
-
+coefs[c(4,3),c(4,3)]
 
 grangertest(GDPVAR$GDPC1~GDPVAR$CPIAUCSL) #yes
 causality(varmod_gr, cause = "CPIAUCSL")
-
+coefs[c(4,5),c(4,5)]
 
 grangertest(GDPVAR$GDPC1~GDPVAR$M1REAL) #yes
 causality(varmod_gr, cause = "M1REAL")
-
+coefs[c(4,6),c(4,6)]
 
 grangertest(GDPVAR$GDPC1~GDPVAR$`S&P 500`) #yes
 causality(varmod_gr, cause = "S.P.500")
+coefs[c(4,7),c(4,7)]
 
 #task e
 
@@ -148,12 +165,47 @@ for (i in 25:250) {
   varfc3 <- append(varfc3,forecasts3$fcst$GDPC1[1])
 }
 
-rmse(project1data_model[25:250],varfc3)
+rmse(project1data_model[26:251],varfc3)
 
 ts.plot(project1data_model[1:251])
 points(arfc, type = "l", col = 2, lty = 2)
 points(varfc, type = "l", col = 3, lty = 2)
-points(varfc, type = "l", col = 4, lty = 2)
+points(varfc3, type = "l", col = 4, lty = 2)
 
 
+data_varp_forecast_eo <- data.frame(project1data_final$sasdate[26:251],project1data_final$GDPC1[26:251],varfc3)
+head(data_forecast_eo) #9/1/1959
+head(data_var_forecast_eo) #6/1/1961 7 0s
+head(data_varp_forecast_eo) #23 0s
+
+colnames(data_forecast_eo) <- c("dates","gdp_real", "ar_forecast")
+final_ar <- data.frame(data_forecast_eo)
+var_missing <- data.frame(project1data_final$sasdate[3:9], 0, 0)
+colnames(var_missing) <- c("dates", "gdp_real", "var_forecast")
+colnames(data_var_forecast_eo) <- c("dates","gdp_real", "var_forecast")
+final_var <- rbind(data_var_forecast_eo,var_missing )
+final_var <- final_var%>%
+  as.Date(final_var$dates, "%m/%d/%Y")%>%
+  arrange(dates)
+varp_missing <- data.frame(project1data_final$sasdate[3:25], 0,0)
+colnames(varp_missing) <- c("dates", "gdp_real", "varp_forecast")
+colnames(data_varp_forecast_eo) <- c("dates","gdp_real", "varp_forecast")
+final_varp <- rbind(data_var_forecast_eo,var_missing )
+
+final_plot <- data.frame(final_ar, final_var, final_varp)
+final_plot$dates <- as.Date(final_plot$dates, "%m/%d/%Y")
+
+varp_plot_total <- ggplot(final_plot, aes(dates)) +  
+  geom_line(aes(y = gdp_real, colour = "actual GDP growth"))
+  geom_line(aes(y = ar_forecast, colour = "AR(1) forecasted GDP growth")) +
+  geom_line(aes(y = var_forecast, colour = "VAR(1) forecasted GDP growth")) +
+  geom_line(aes(y = varp_forecast, colour = "VAR(3) forecasted GDP growth")) +
+  ggtitle("Forecast of GDP Growth") + theme(plot.title = element_text(hjust = 0.5)) +
+  xlab("Time") + ylab("actual GDP growth/ forecasted GDP growth in %") +
+  theme(legend.position="bottom")
+
+  plot(final_plot$gdp_real, type = "l")
+  points(final_plot$ar_forecast, type = "l", col = 2, lty = 2)
+  points(final_plot$var_forecast, type = "l", col = 3, lty = 2)
+  points(final_plot$varp_forecast, type = "l", col = 4, lty = 2)
 
