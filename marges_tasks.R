22b2e79b5f78bbc430454929abfd0966dbe895cd
pinderimargesi@gmail.com <pinderimargesi@gmail.com>
2022-05-12 12:49:07 +0200

    fixed some mistakes

--- marges_tasks.R
+++ marges_tasks.R
@@ -1,6 +1,7 @@
 library(tidyverse)
 library(writexl)
-
+library(Metrics)
+library(readr)
 
 X2022_02 <- read_csv("2022-02.csv")
 excel_file <- X2022_02[3:254,] %>%  dplyr::select(sasdate,GDPC1,CUMFNS,UNRATESTx,CPIAUCSL,FEDFUNDS,M1REAL,'S&P 500')
@@ -31,7 +32,7 @@ length(alltogether$GDPC1_gr[-1])
 models <- list()
 
 for (i in 3:251) {
-  models[[i]]<- ar.ols(alltogether$GDPC1_gr[-1][1:i] , 
+  models[[i]]<- ar.ols(alltogether$GDPC1_gr[2:252][1:i] , 
                        order.max = 1)
   
 }
@@ -40,18 +41,40 @@ predictions <-list()
 prediction <- predict(model1,n.ahead = 1)                        
 
 
-for (i in 3:251) {
-  predictions[[i]] <- predict(models[[i]],n.ahead = 1)
+for (i in 2:251) {
+  predictions[[i-1]] <- predict(models[[i]],n.ahead = 1)
   
 }
 
 predictions[[4]][[1]][1]
 
-predicted<-vector(length = 249)
-for (i in 1:249) {
-  predicted[i]<- predictions[[i+2]][[1]][1]
+predicted<-vector()
+for (i in 1:250) {
+  predicted[i]<- predictions[[i]][[1]][1]
   
 }
 
-library(Metrics)
-rmse(alltogether$GDPC1_gr[-c(1,2,3)], predicted)
+
+rmse(alltogether$GDPC1_gr[3:252], predicted)
+
+alltogether$predictedgdpar_1[3:252]<-predicted
+
+plot_file<-ts(plot_file,start=1959, frequency = 4)
+
+
+autoplot(plot_file[,])+
+  ggtitle("Capital Utilization", subtitle = "Capacity Utilization: Manufacturing (SIC) (Percent of Capacity)") +
+  xlab("Year") + ylab("Percentage utilized")
+
+
+
+# forecast <- c()
+# for(i in 2:length(data$GDP_per)) {
+#   GDP_ar<-arima(data$GDP_per[1:i],c(1,0,0))
+#   GDP_ar_forecast <- predict(GDP_ar, n.ahead = 1)
+#   new_value <- GDP_ar_forecast$pred           
+#   forecast <- c(forecast, new_value)    
+# }
+
+
+ggp
